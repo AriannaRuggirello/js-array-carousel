1927cafce4b27ef73b3ea2f5cf45d04411f3d675
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-05-04 19:36:43 +0200

    layout style update with arrow

--- js/main.js
+++ js/main.js
@@ -8,5 +8,23 @@
 //MILESTONE 2 Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell’array fornito e un semplice ciclo for che concatena un template literal. Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile. Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
 
 
+    // seleziono le foto per poterle gestire
+
+    // imposto un indice per segnare la foto attiva al momento
+
+    // seleziono la freccia che permetterà di mostrare le img una dopo l'altra
+
+ 
 
 // MILESTONE 3 Al click dell’utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+
+
+    // funzione al click
+
+    // togli la classe active all'elemento attivo in quel momento
+
+    // passare alla foto successiva
+
+    // alla foto successiva aggiungere la classe active
+
+
